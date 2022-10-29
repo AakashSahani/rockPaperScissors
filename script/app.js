1371c29b9248b88f50eb4cff7c9a9c6385955b87
Aakashsahani <sahani.aakash120@gmail.com>
2022-10-28 22:32:31 -0400

    Added Game logic

---
+++ script/app.js
@@ -0,0 +1,57 @@
+//Choices
+const paper = document.querySelector('.paper');
+const rock = document.querySelector('.rock');
+const scissor = document.querySelector('.scissor');
+
+//Winning Combination
+const winningCombo = [
+	['paper', 'rock'],
+	['rock', 'scissor'],
+	['scissor', 'paper'],
+];
+
+//Player and house variables
+let player, house;
+
+//Event Listeners
+paper.addEventListener('click', () => {
+	player = 'paper';
+	house = houseChoice('rock', 'scissor');
+	whoWon(player, house);
+});
+
+rock.addEventListener('click', () => {
+	player = 'rock';
+	house = houseChoice('paper', 'scissor');
+	whoWon(player, house);
+});
+
+scissor.addEventListener('click', () => {
+	player = 'scissor';
+	house = houseChoice('paper', 'rock');
+	whoWon(player, house);
+});
+
+//Filters winningCombo
+function whoWon(player, house) {
+	if (
+		winningCombo.filter((combo) => combo[0] === player && combo[1] === house)
+			.length === 0
+	) {
+		console.log('You lost');
+	} else {
+		console.log('You won');
+	}
+}
+
+//Returns a random choice for House
+function houseChoice(choice1, choice2) {
+	let random = Math.floor(Math.random() * 2);
+	if (random === 1) {
+		console.log('House chose ' + choice1);
+		return choice1;
+	} else {
+		console.log('House chose ' + choice2);
+		return choice2;
+	}
+}
